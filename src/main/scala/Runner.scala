6a47d1f6559accfcd128df00be3540c6aa1518fe
Tomasz Pewiński <pewniak747@gmail.com>
2014-05-01 22:25:36 +0200

    range based algorithms

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -9,23 +9,29 @@ object Runner {
 
     def charToInt(a: Char): Int = a.toInt - 48
 
-    def level(a: Int): Int = if(a <= 2) return 0
-      else if (a <= 6) return 1
-      else return 2
-
     val pattern = List(0, 0, 0, 0, 0, 3, 3, 9, 9, 9, 6, 0, 0, 0,
                        0, 0, 0, 3, 4, 9, 5, 9, 9, 9, 9, 4, 0, 0,
                        0, 1, 2, 7, 5, 9, 5, 9, 9, 9, 9, 7, 1, 0,
                        3, 9, 5, 9, 5, 9, 5, 9, 9, 9, 9, 9, 9, 3,
                        6, 9, 5, 9, 5, 9, 5, 9, 9, 9, 9, 9, 9, 6,
                        1, 8, 5, 9, 5, 9, 5, 9, 9, 9, 9, 9, 8, 1)
-    val weights = pattern.map((d) => if(level(d) == 0) 5 else 1)
+
+    val white = (0, 2)
+    val gray  = (3, 9)
+    val black = (6, 9)
+    val ranges = List(white, white, white, white, white, gray,  gray,  black, black, black, black, white, white, white,
+                      white, white, white, gray,  gray,  gray,  gray,  black, black, black, black, white, white, white,
+                      white, white, gray,  gray,  gray,  gray,  gray,  black, black, black, black, black, white, white,
+                      gray,  gray,  gray,  gray,  gray,  gray,  gray,  black, black, black, black, black, black, black,
+                      gray,  gray,  gray,  gray,  gray,  gray,  gray,  black, black, black, black, black, black, black,
+                      white, gray,  gray,  gray,  gray,  gray,  gray,  black, black, black, black, black, black, white)
+
 
     val chunkSize = pattern.length
     Source.stdin.take(2) // skip 3. part of 3.141592
     var window = ArrayBuffer[Int]()
     window ++= Source.stdin.take(pattern.length).map(charToInt(_))
-    var minimum = 10 * pattern.length
+    var maximum = 0
     var results = Array[Int]()
 
     var iterations = 0
@@ -37,14 +43,14 @@ object Runner {
       window += digit
       window = window.drop(1)
 
-      val score = (window, pattern, weights).zipped.foldLeft(0)((mem, triple) =>
-        triple match {
-          case (digit1, digit2, weight) => mem + weight * Math.abs(level(digit1) - level(digit2))
-        }
-      )
-      if (minimum >= score) {
-        results = (results :+ score).sorted.take(10)
-        minimum = results.last
+      val score = ranges.zip(window).filter({
+        case ((lowerBound, upperBound), pixel) => lowerBound <= pixel && pixel <= upperBound
+      }).length
+
+      if (maximum <= score) {
+        results = (results :+ score).sorted.reverse.take(10)
+        maximum = results.last
+        println("Result set: " + results.mkString(", "))
         println("Score: " + score + "; Sequence: " + window.mkString + "; Offset: " + iterations)
       }
     }
