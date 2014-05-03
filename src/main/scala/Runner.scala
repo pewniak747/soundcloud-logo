53869d2b302d6f5bd73b33959b1d33ac5bd0fe8f
Tomasz Pewiński <pewniak747@gmail.com>
2014-05-03 14:19:25 +0200

    calculate changes in score

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -9,44 +9,87 @@ object Runner {
 
     def charToInt(a: Char): Int = a.toInt - 48
 
-    val pattern = List(0, 0, 0, 0, 0, 3, 3, 9, 9, 9, 6, 0, 0, 0,
-                       0, 0, 0, 3, 4, 9, 5, 9, 9, 9, 9, 4, 0, 0,
-                       0, 1, 2, 7, 5, 9, 5, 9, 9, 9, 9, 7, 1, 0,
-                       3, 9, 5, 9, 5, 9, 5, 9, 9, 9, 9, 9, 9, 3,
-                       6, 9, 5, 9, 5, 9, 5, 9, 9, 9, 9, 9, 9, 6,
-                       1, 8, 5, 9, 5, 9, 5, 9, 9, 9, 9, 9, 8, 1)
-
     val white = (0, 2)
     val gray  = (3, 9)
     val black = (6, 9)
-    val ranges = List(white, white, white, white, white, gray,  gray,  black, black, black, black, white, white, white,
-                      white, white, white, gray,  gray,  gray,  gray,  black, black, black, black, white, white, white,
-                      white, white, gray,  gray,  gray,  gray,  gray,  black, black, black, black, black, white, white,
-                      gray,  gray,  gray,  gray,  gray,  gray,  gray,  black, black, black, black, black, black, black,
-                      gray,  gray,  gray,  gray,  gray,  gray,  gray,  black, black, black, black, black, black, black,
-                      white, gray,  gray,  gray,  gray,  gray,  gray,  black, black, black, black, black, black, white)
-    val weights = pattern.map((d) => if(d < 2) 5 else 1)
+    val pattern = Array(0, 0, 0, 0, 0, 3, 3, 9, 9, 9, 6, 0, 0, 0,
+                        0, 0, 0, 3, 4, 9, 5, 9, 9, 9, 9, 4, 0, 0,
+                        0, 1, 2, 7, 5, 9, 5, 9, 9, 9, 9, 7, 1, 0,
+                        3, 9, 5, 9, 5, 9, 5, 9, 9, 9, 9, 9, 9, 3,
+                        6, 9, 5, 9, 5, 9, 5, 9, 9, 9, 9, 9, 9, 6,
+                        1, 8, 5, 9, 5, 9, 5, 9, 9, 9, 9, 9, 8, 1)
+
+    val ranges = Array(white, white, white, white, white, gray,  gray,  black, black, black, black, white, white, white,
+                       white, white, white, gray,  gray,  gray,  gray,  black, black, black, black, white, white, white,
+                       white, white, gray,  gray,  gray,  gray,  gray,  black, black, black, black, black, white, white,
+                       gray,  gray,  gray,  gray,  gray,  gray,  gray,  black, black, black, black, black, black, black,
+                       gray,  gray,  gray,  gray,  gray,  gray,  gray,  black, black, black, black, black, black, black,
+                       white, gray,  gray,  gray,  gray,  gray,  gray,  black, black, black, black, black, black, white)
+
+    val weights = ranges.map((r) => if(r == white) 5 else 1)
+
+    val breakpoints: Array[Int] = ranges.sliding(2).zipWithIndex.map {
+      case (ranges, index) if ranges(0) != ranges(1) => index
+      case _ => -1
+    }.filter(_ >= 0).toArray
+
+    def scoreOfPixel(pixel: Int, range: (Int, Int), weight: Int) = range match {
+      case (lowerBound, upperBound) => if(lowerBound <= pixel && pixel <= upperBound) weight else 0
+    }
+
+    def scoresOfSequence(pixels: Seq[Int]) = (pixels, ranges, weights).zipped.map {
+      case (pixel, range, weight) => scoreOfPixel(pixel, range, weight)
+    }.toArray
+
+    if(args.size == 1) {
+      val input = args(0).map(charToInt(_))
+      val score = scoresOfSequence(input).sum
+      println("Score: " + score)
+      System.exit(0)
+    }
 
     val chunkSize = pattern.length
-    Source.stdin.take(2) // skip 3. part of 3.141592
+    val stdin = Source.stdin
+    stdin.drop(2) // skip 3. part of 3.141592
+
     var window = ArrayBuffer[Int]()
-    window ++= Source.stdin.take(pattern.length).map(charToInt(_))
+    window ++= stdin.take(pattern.length).map(charToInt(_))
+
+    var scores = ArrayBuffer[Int]()
+    scores ++= scoresOfSequence(window)
+
     var maximum = 0
     var results = Array[Int]()
 
+    //println("Breakpoints: " + breakpoints.mkString(" "))
+    //println("Pre digits:  " + window.mkString(" "))
+    //println("Pre scores:  " + scores.mkString(" "))
+
     var iterations = 0
-    for(chunk <- Source.stdin.grouped(chunkSize); character <- chunk) {
+    for(chunk <- stdin.grouped(chunkSize); character <- chunk) {
       iterations += 1
       if(iterations % (1024 * 1024) == 0) println("Read " + iterations + " digits")
 
       val digit = charToInt(character)
-      window += digit
+
+      // calculate changed pixel scores
+      breakpoints.foreach { (breakpoint) =>
+        scores(breakpoint + 1) = scoreOfPixel(window(breakpoint + 1), ranges(breakpoint), weights(breakpoint))
+      }
+
+      // first pixel goes out
       window = window.drop(1)
+      scores = scores.drop(1)
 
-      val score = (ranges, window, weights).zipped.map({
-        case ((lowerBound, upperBound), pixel, weight) => if(lowerBound <= pixel && pixel <= upperBound) weight else 0
-      }).sum
+      // last pixel goes in
+      scores += scoreOfPixel(digit, ranges.last, weights.last)
+      window += digit
 
+      //println("Post scores: " + scores.mkString(" "))
+      //println("Post soseq:  " + scoresOfSequence(window).mkString(" "))
+      //println("Post digits: " + window.mkString(" "))
+
+      val score = scores.sum
       if (maximum <= score) {
         results = (results :+ score).sorted.reverse.take(10)
         maximum = results.last
